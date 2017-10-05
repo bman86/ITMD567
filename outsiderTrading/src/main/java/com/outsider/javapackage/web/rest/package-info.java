98aac75c3fa0afd3742c1b6f58ddd96f7dae9184
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-10-04 22:56:19 -0500

    jhipster

---
+++ outsiderTrading/src/main/java/com/outsider/javapackage/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.outsider.javapackage.web.rest;
---
+++ outsiderTrading/src/main/java/com/outsider/javapackage/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.outsider.javapackage.web.rest.vm;
