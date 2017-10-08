2d32e8759a3bb2b6cba80db6a2a3c905bf626f3c
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-10-08 16:20:03 -0500

    Generated with JHipster 4.7.0

---
+++ outsiderTrading/src/main/java/com/outsider/javapackage/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.outsider.javapackage.domain;
---
+++ outsiderTrading/src/main/java/com/outsider/javapackage/security/social/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring social configuration.
+ */
+package com.outsider.javapackage.security.social;
