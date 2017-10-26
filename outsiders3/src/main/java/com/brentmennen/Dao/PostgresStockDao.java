e8433897e81da820695b256e4c88d073e157d0e6
Brent Mennen <brentmennen@Brents-MacBook-Pro.local>
2017-10-25 19:15:06 -0500

    Connected with AWS and create, update, edit, delete finished

--- outsiders3/src/main/java/com/brentmennen/Dao/PostgresStockDao.java
+++ outsiders3/src/main/java/com/brentmennen/Dao/PostgresStockDao.java
@@ -1,5 +1,6 @@
 package com.brentmennen.Dao;
 
+
 import com.brentmennen.Entity.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,46 +18,68 @@ public class PostgresStockDao implements StockDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private static class StockRowMapper implements RowMapper<Stock>{
+
+        @Override
+        public Stock mapRow(ResultSet resultSet, int i) throws SQLException {
+            Stock stock = new Stock();
+            stock.setId(resultSet.getInt("id"));
+            stock.setSymbl(resultSet.getString("symbl"));
+            stock.setOwn(resultSet.getBoolean("own"));
+            stock.setEntryPrice(resultSet.getDouble("entryPrice"));
+            stock.setUp(resultSet.getBoolean("up"));
+            stock.setTarget(resultSet.getDouble("target"));
+            stock.setTime(resultSet.getString("time"));
+            return stock;
+        }
+    }
     @Override
     public Collection<Stock> getAllStocks() {
-//        SELECT select_list FROM table_expression [sort_specification]
+
         final String sql = "SELECT id, symbl, own, entryPrice, up, target, time FROM outsiders";
-        List<Stock> stocks = jdbcTemplate.query(sql, new RowMapper<Stock>() {
-            @Override
-            public Stock mapRow(ResultSet resultSet, int i) throws SQLException {
-                Stock stock = new Stock();
-                stock.setId(resultSet.getInt("id"));
-                stock.setSymbl(resultSet.getString("symbl"));
-                stock.setOwn(resultSet.getBoolean("own"));
-                stock.setEntryPrice(resultSet.getDouble("entryPrice"));
-                stock.setUp(resultSet.getBoolean("up"));
-                stock.setTarget(resultSet.getDouble("target"));
-                stock.setTime(resultSet.getString("time"));
-                return stock;
-            }
-        });
+        List<Stock> stocks = jdbcTemplate.query(sql, new StockRowMapper());
 
         return stocks;
     }
 
-
     @Override
     public Stock getStockById(int id) {
-        return null;
+        final String sql = "SELECT id, symbl, own, entryPrice, up, target, time FROM outsiders WHERE id = ?";
+        final Stock stock = jdbcTemplate.queryForObject(sql, new StockRowMapper(), id);
+
+        return stock;
     }
 
     @Override
     public void removeStockById(int id) {
-
+        final String sql = "DELETE FROM outsiders WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
     @Override
     public void updateStock(Stock stock) {
+        final String sql = "UPDATE outsiders SET symbl = ?, own = ?, entryPrice = ?, up = ?, target = ?, time = ? WHERE id = ?";
+        final int id = stock.getId();
+        final String symbl = stock.getSymbl();
+        final Boolean own = stock.getOwn();
+        final Double entryPrice = stock.getEntryPrice();
+        final Boolean up = stock.getUp();
+        final Double target = stock.getTarget();
+        final String time = stock.getTime();
+        jdbcTemplate.update(sql, new Object[] {symbl, own, entryPrice, up, target, time, id});
 
     }
 
     @Override
     public void insertStockToDb(Stock stock) {
+        final String sql = "INSERT INTO outsiders (symbl, own, entryPrice, up, target, time) VALUES (?, ?, ?, ?, ?, ?)";
+        final String symbl = stock.getSymbl();
+        final Boolean own = stock.getOwn();
+        final Double entryPrice = stock.getEntryPrice();
+        final Boolean up = stock.getUp();
+        final Double target = stock.getTarget();
+        final String time = stock.getTime();
+        jdbcTemplate.update(sql, new Object[] {symbl, own, entryPrice, up, target, time});
 
     }
 }
